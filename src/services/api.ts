47055bc565eaa0ef151f4b934547214c35e2e33c
Vinicius <viniciusgrp@gmail.com>
2023-03-25 15:13:36 -0300

    change baseurl from local to render deploy

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,9 @@
 import axios from "axios";
 
-export const api = axios.create({baseURL: 'http://localhost:3000', headers: {
-    "Content-Type": "application/json"
-}})
+export const api = axios.create({
+  baseURL: "https://clientsbook.onrender.com",
+//   baseURL: "http://localhost:3000",
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
